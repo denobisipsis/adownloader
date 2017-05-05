00fa9707d069ee216be6c21fa46702f35e9aeef5
denobisipsis <denobisipsis@users.noreply.github.com>
2017-05-05 18:17:29 +0200

    Update puremd5PHP.php

--- puremd5PHP.php
+++ puremd5PHP.php
@@ -1,195 +1,96 @@
 <?
 /*
-
 This is an implementation of the MD5 hash function, as specified by
 RFC 1321, in pure PHP. It was implemented using Bruce Schneier's
 excellent book "Applied Cryptography", 2nd ed., 1996.
 
-Pure php md5 implementation
-
 I have simplified the main iteration
 
 @denobisipsis 2017
-
-
 ;)
 */
-function MD($string)
-    {
-    $A = $a = "67452301";
-    $B = $b = "efcdab89";
-    $C = $c = "98badcfe";
-    $D = $d = "10325476";
-    
-    $words = str2blks_MD5($string);   
-    
-    $torot=array("A","B","C","D");    
-
-    $it=array(7,12,17,22,5,9,14,20,4,11,16,23,6,10,15,21);
-
-    $funcs=array("F","G","H","I");    
-	       
-    $moduls=array(0,1,1,5,5,3,0,7);
-		
-    $i = 0;
-            
-    while   ($i++ < 64)
-    	   {
-    	   $acs[$i-1]=dechex(abs(sin($i))* 4294967296);				
-    	   } 
-            
-    for($i = 0; $i < count($words)/16; $i++)
-	    {
-            $a  = $A;
-            $b  = $B;
-            $c  = $C;
-            $d  = $D;
-            $n  = 0; 
-	    
-	    for ($rot3=0;$rot3<4;$rot3++)
-	    	{
-	    	$minit=$moduls[$rot3*2];
-	   	$madd=$moduls[$rot3*2+1];	
-							
-	    	for ($rot2=0;$rot2<4;$rot2++)
-		    	{
-		    	for ($rot=0;$rot<4;$rot++)
-			    {
-			    $word=$words[$minit + ($i * 16)];
-			    $nit=$it[$rot+4*$rot3];
-									
-			    FGHI (${"$torot[0]"}, ${"$torot[1]"}, ${"$torot[2]"}, ${"$torot[3]"}, $word, $nit, $acs[$n],$funcs[$rot3]); 
-									
-		    	    array_unshift($torot,$torot[3]);
-		            array_pop($torot);
-									
-			    $minit=($minit+$madd)%16;
-			    ++$n;
-			    }	    
-		    	 }
-	    	}
-	    
-            $A=AddUnsigned(hexdec2($A),hexdec2($a));
-            $B=AddUnsigned(hexdec2($B),hexdec2($b));
-            $C=AddUnsigned(hexdec2($C),hexdec2($c));
-            $D=AddUnsigned(hexdec2($D),hexdec2($d));    
-    	    }
-
-   	return WordToHex($A).WordToHex($B).WordToHex($C).WordToHex($D);
-	}
-
+class MD
+{
 function WordToHex($lValue) 
-   { 
-    $WordToHexValue = "";
-    for ($lCount = 0;$lCount<4;$lCount++) 
-        { 
-        $lByte = hexdec2($lValue)>>($lCount*8) & 255; 
-        $WordToHexValue.= sprintf("%02x",$lByte);
-        }
+    { 
+    while ($lCount<4) 
+        $WordToHexValue.= sprintf("%02x",$lValue>>($lCount++*8) & 255);
     return $WordToHexValue;
-    }
-
-function FGHI(&$A, $B, $C, $D, $M, $s, $t, $func)
-        {
-        $Level1 = hexdec2(AddUnsigned(FGHI2($B, $C, $D, $func) , bindec($M) ));
-        $level2 = hexdec2(AddUnsigned($Level1, hexdec2($t)));  
-        $A = hexdec2(AddUnsigned(hexdec2($A),$level2));
-        $A = rotate($A, $s); 
-        $A = AddUnsigned($A , hexdec2($B)) ; 
-        }
-
-function FGHI2($X, $Y, $Z,$func)
-        {    
-        $X = hexdec2($X); 
-        $Y = hexdec2($Y);
-        $Z = hexdec2($Z);
-        
-	switch ($func)
-		{
-		case "F":
-	        	$calc = (($X & $Y) | ((~ $X) & $Z));break;
-		case "G":
-			$calc = (($X & $Z) | ($Y & (~ $Z)));break;
-		case "H":
-			$calc = ($X ^ $Y ^ $Z);break;
-		case "I":
-			$calc = ($Y ^ ($X | (~ $Z)));
-		}
-        return  $calc; 
-  	}
-
-function dectohex($res)
-	{
-        if($res < 0)
-            return '-'.dechex(abs($res));
-
-        return dechex($res);	
-	}
-	
-function hexdec2($hex)
-    {  
-    if($hex[0] == "-")   
-        return doubleval('-'.hexdec(str_replace("-", "", $hex )));
-    
-    return hexdec($hex);
-    }
-	
+    }		
 function AddUnsigned($lX,$lY) 
-   { 
+    {   
     $lX8 = ($lX & 0x80000000);
     $lY8 = ($lY & 0x80000000);
     $lX4 = ($lX & 0x40000000);
-    $lY4 = ($lY & 0x40000000);
-    
-    $lResult = ($lX & 0x3FFFFFFF)+($lY & 0x3FFFFFFF);
-    
-    $res=$lResult ^ $lX8 ^ $lY8;
-    
-    if ($lX4 & $lY4)                       return dectohex($res ^ 0x80000000);   
-    if ($lX4 | $lY4) 
-	{
-        if ($lResult & 0x40000000)         return dectohex($res ^ 0xC0000000);
-        else                               return dectohex($res ^ 0x40000000);       
-    	}                                  return dectohex($res);
+    $lY4 = ($lY & 0x40000000);    
+    $lResult = ($lX & 0x3FFFFFFF)+($lY & 0x3FFFFFFF);    
+    $res = $lResult ^ $lX8 ^ $lY8;    
+    if      ($lX4 & $lY4)                  $res ^= 0x80000000;   
+    else if ($lX4 | $lY4) 
+		{
+        	if ($lResult & 0x40000000) $res ^= 0xC0000000;
+        	else                       $res ^= 0x40000000;       
+    		}                             
+    return $res;	    
     }
-
-function rotate ($decimal, $bits) 
-    { 
-    return  (($decimal << $bits) |  shiftright($decimal, (32 - $bits))  & 0xffffffff);
-    }
-
 function shiftright($decimal , $right)
     { 
-    $shift=$decimal >> $right;
-    
+    $shift=$decimal >> $right;    
     if($decimal >= 0) return $shift;
-
     return bindec(substr(decbin($shift),$right));
-    }
-    	
-function str2blks_MD5($str)
-  {
-  $nblk = ((strlen($str) + 8) >> 6) + 1;
+    }		
+function calc($string)
+	{
+	$A = 1732584193;$B = -271733879;$C = ~$A;$D = ~$B;;
 	
-  $blks = array($nblk * 16);
+	$k=strlen($string);$words=array();	  
+	  	
+	for($m=0;$k>$m;) $words[$m>>2]|=ord($string[$m]) << (8 * ($m++ % 4));
 	
-  for($i = 0; $i < $nblk * 16; $i++) 
-				$blks[$i] = 0;
-				
-  for($i = 0; $i < strlen($str); $i++)
-    		$blks[$i >> 2] |= ord($str[$i]) << (($i % 4) * 8);
-				
-  $blks[$i >> 2] |= 0x80 << (($i % 4) * 8);
-	
-  $blks[$nblk * 16 - 2] = strlen($str) * 8;
-	
-  for ($i=0; $i < $nblk * 16; $i++) 			
-      	$blks[$i] = decbin($blks[$i]);
-           
-  return $blks;
-  }
-
-$str='test';
-echo md5($str); // 098f6bcd4621d373cade4e832627b4f6
-echo'<br>';
-echo MD($str); //  098f6bcd4621d373cade4e832627b4f6
+	$words[$m >> 2] |= 0x80 << (($m % 4) * 8);		
+	$words[($wl=(($k+8)>>6)*16+14)] = 8 * $k;	
+	$vr=$wr=array("A","B","C","D");    	
+	$it=array(7,12,17,22,5,9,14,20,4,11,16,23,6,10,15,21);	       
+	$moduls=array(0,1,1,5,5,3,0,7); 			
+			               
+	for($i = 0; $i < $wl/16; $i++)
+		    {	    
+		    $w=-1;while($w++<4) ${chr($w+65+32)}=${chr($w+65)};
+		    
+		    for ($n=0;$n<64;$n++)
+			{
+			$m = $n >> 4;
+							    				
+			$minit=$moduls[($m % 16) * 2];
+			$madd =$moduls[($m % 16) * 2+1];				    					    				
+															
+		        $X = ${"$vr[1]"}; 
+		        $Y = ${"$vr[2]"};
+		        $Z = ${"$vr[3]"};
+			
+			$t = sprintf("%'.04d",decbin(1 << $m));
+		        
+			$calc = (($X&$Y)|((~$X)&$Z))*$t[3] | 
+				(($X&$Z)|($Y&(~$Z)))*$t[2] | 
+				($X^$Y^$Z)*$t[1] | 
+				($Y^($X|(~$Z)))*$t[0];
+						 													
+			$level12 = $this->AddUnsigned(
+					$this->AddUnsigned($calc,$words[($minit+$madd*$n)%16+($i*16)])
+					,abs(sin($n+1))*4294967296); 
+			
+		        ${"$vr[0]"} = $this->AddUnsigned((${"$vr[0]"}),$level12);			
+			${"$vr[0]"} = (${"$vr[0]"}<<($bits=$it[4*$m+$n%4]))|
+					$this->shiftright(${"$vr[0]"},(32 - $bits));
+		        ${"$vr[0]"} = $this->AddUnsigned(${"$vr[0]"},(${"$vr[1]"}));
+								
+			array_pop(array_unshift($vr,$vr[3]));
+			}
+		    
+		    foreach ($wr as $w) ${"$w"}=$this->AddUnsigned(${"$w"},${chr(ord($w)+32)});	      		    		    
+	    	    }
+		
+	return $this->WordToHex($A).$this->WordToHex($B).$this->WordToHex($C).$this->WordToHex($D);
+	}
+}
+$str='test';$a=new MD();echo md5($str).'<br>'.$a->calc($str);
