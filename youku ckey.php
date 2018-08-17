f588fb41f5cb6b4a26faab02ed855fb0c364d868
denobisipsis <denobisipsis@users.noreply.github.com>
2018-08-17 18:54:23 +0200

    Create youku ckey.php

---
+++ youku ckey.php
@@ -0,0 +1,15 @@
+<?
+
+/*
+YOUKU CKEY algorithm hacked 
+
+Youku uses a ckey algorithm to get all parts and qualities. Without that, you only get some parts of large videos. With Adownloader v19
+
+http://www.mediafire.com/file/u1ydjy811d1eaot/a3toMP4v19.exe/file
+
+you can get this ckey parameter.
+
+
+*/
+
+?>
